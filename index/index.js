3fce3fd40ed24df11fec2d0f7215300b6e6306a4
Lohit Aditya <bp98488@gmail.com>
2023-03-06 16:02:23 +0530

    Add files via upload

---
+++ index/index.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Cyber Security Researcher",
+    "Content Creator", "TryHackMe top 1%"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
